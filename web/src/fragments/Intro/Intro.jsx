04db94b4e02e577ea9d30f55d7b340dbc56772cf
AstraL <sawyer126@gmail.com>
2022-07-07 15:41:28 +0300

    two more buttons link

--- web/src/fragments/Intro/Intro.jsx
+++ web/src/fragments/Intro/Intro.jsx
@@ -3,19 +3,24 @@ import styles from './Intro.module.css'
 import background from '../../assets/Intro_bg.svg'
 
 export const Intro = () => {
+  const handleClick = e => {
+    e.preventDefault();
+    window.location.href = '/#footer';
+  };
+
   return (
     <div className={styles.container}>
         <img className={styles.background} src={background} alt="Background" />
 
         <p className={styles.title}>
-            Take advantage of the ONLY secured investment 
-            opportunity in the cannabis industry 
+            Take advantage of the ONLY secured investment
+            opportunity in the cannabis industry
             and earn up to <span className={styles.title_span}>10X returns</span>
         </p>
-        
+
         <div className={styles.buttons}>
-            <button className={styles.button}>INVESTOR DECK</button>
-            <button className={styles.button}>INVEST NOW</button>
+            <button onClick={handleClick} className={styles.button}>INVESTOR DECK</button>
+            <button onClick={handleClick} className={styles.button}>INVEST NOW</button>
         </div>
     </div>
   )
