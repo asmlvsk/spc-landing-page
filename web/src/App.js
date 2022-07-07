e5220202e83186694b9ac89cd74fd8e3dd2c8093
AstraL <sawyer126@gmail.com>
2022-07-07 14:53:48 +0300

    document title

--- web/src/App.js
+++ web/src/App.js
@@ -7,10 +7,14 @@ import { Equity } from './fragments/Equity/Equity';
 import { AssetsSection } from './fragments/AssetsSection/AssetsSection';
 import { Footer } from './fragments/Footer/Footer';
 import { IntroductionPanel } from './fragments/Intro/IntroductionPanel';
+import { Helmet } from 'react-helmet'
 
 function App() {
   return (
     <div className="container">
+        <Helmet>
+          <title>{ 'Sophia\'s Presidents Club' }</title>
+        </Helmet>
         <Navbar/>
         <Intro/>
         <IntroductionPanel/>
